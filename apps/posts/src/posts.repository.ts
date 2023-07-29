2048bad5178a7943c73a4aeee36f8c08c2b4ae0d
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-07-29 15:51:44 +0330

    new [posts service] -> create posts repository to manage app data

---
+++ apps/posts/src/posts.repository.ts
@@ -0,0 +1,64 @@
+import { UUID, randomUUID } from 'crypto';
+import { Injectable } from '@nestjs/common';
+import { Post } from './posts.interface';
+
+@Injectable()
+export class PostsRepository {
+  private repository: Post[];
+
+  constructor() {
+    this.repository = [];
+  }
+
+  insertOne(instance: Omit<Post, 'id'>): Post {
+    const newPost = { id: randomUUID(), ...instance };
+
+    this.repository.push(newPost);
+
+    return newPost;
+  }
+
+  updateOne(id: UUID, instance: Partial<Omit<Post, 'id'>>): Post {
+    for (let index = 0; index < this.repository.length; index++) {
+      const post = this.repository[index];
+
+      if (post.id === id) {
+        const updatedPost = { ...post, ...instance };
+
+        this.repository[index] = updatedPost;
+
+        return updatedPost;
+      }
+    }
+
+    return undefined;
+  }
+
+  deleteOne(id: UUID): Post {
+    for (let index = 0; index < this.repository.length; index++) {
+      const post = this.repository[index];
+
+      if (post.id === id) {
+        return this.repository.splice(index, 1)[0];
+      }
+    }
+
+    return undefined;
+  }
+
+  findAll(): Post[] {
+    return [...this.repository];
+  }
+
+  findById(id: UUID): Post {
+    for (let index = 0; index < this.repository.length; index++) {
+      const post = this.repository[index];
+
+      if (post.id === id) {
+        return { ...post };
+      }
+    }
+
+    return undefined;
+  }
+}
