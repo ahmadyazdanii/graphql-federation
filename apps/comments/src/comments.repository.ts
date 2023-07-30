d1c191c9e4c0324e5413053374eddbb20c5e3a05
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-07-30 11:11:01 +0330

    new [comments service] -> create repository to handle data management

---
+++ apps/comments/src/comments.repository.ts
@@ -0,0 +1,64 @@
+import { UUID, randomUUID } from 'crypto';
+import { Injectable } from '@nestjs/common';
+import { Comment } from './comments.interface';
+
+@Injectable()
+export class CommentsRepository {
+  private repository: Comment[];
+
+  constructor() {
+    this.repository = [];
+  }
+
+  insertOne(instance: Omit<Comment, 'id'>): Comment {
+    const newComment = { id: randomUUID(), ...instance };
+
+    this.repository.push(newComment);
+
+    return newComment;
+  }
+
+  updateOne(id: UUID, instance: Partial<Omit<Comment, 'id'>>): Comment {
+    for (let index = 0; index < this.repository.length; index++) {
+      const comment = this.repository[index];
+
+      if (comment.id === id) {
+        const updatedComment = { ...comment, ...instance };
+
+        this.repository[index] = updatedComment;
+
+        return updatedComment;
+      }
+    }
+
+    return undefined;
+  }
+
+  deleteOne(id: UUID): Comment {
+    for (let index = 0; index < this.repository.length; index++) {
+      const comment = this.repository[index];
+
+      if (comment.id === id) {
+        return this.repository.splice(index, 1)[0];
+      }
+    }
+
+    return undefined;
+  }
+
+  findAll(): Comment[] {
+    return [...this.repository];
+  }
+
+  findById(id: UUID): Comment {
+    for (let index = 0; index < this.repository.length; index++) {
+      const comment = this.repository[index];
+
+      if (comment.id === id) {
+        return { ...comment };
+      }
+    }
+
+    return undefined;
+  }
+}
